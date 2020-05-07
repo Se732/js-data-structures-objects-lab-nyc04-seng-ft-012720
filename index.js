81d3a816e85c57597d1966e347f12a6f6135d859
Se732 <wesley.m.perez@gmail.com>
2020-05-07 17:53:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution in this file!
+// Write your solution in this file
+//set variable to an empty object 
+let driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value)
+// pass in argument of and object,key, and value
+// in this case the object is saved to the variable driver 
+{
+// make new varible and set the value to a copy of the original object
+    let newDriver = {...driver}
+// new varible set key = value
+    newDriver[key] = value 
+
+    return newDriver 
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
+{
+    driver[key] = value 
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key)
+{
+    let newDriver = Object.assign({}, driver)
+    
+    delete newDriver[key]
+
+    return newDriver
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key)
+{
+    delete driver[key]
+
+    return driver 
+}
